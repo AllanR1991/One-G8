4c2690c324bca9a5652733e8ac9791bf5db28c82
Allan Rodrigues dos Santos <allanrodrigues1991.ar@gmail.com>
2025-01-18 03:22:39 -0300

    refactor: refatorar lógica do jogo do número secreto e melhorar a interação com o usuário

--- 01 - Lógica de programação mergulhe em programação com JavaScript/01/logica-js-projeto_inicial/app.js
+++ 01 - Lógica de programação mergulhe em programação com JavaScript/01/logica-js-projeto_inicial/app.js
@@ -1,22 +1,22 @@
 alert('Boas vindas ao jogo do número secreto.');
 
+let chute;
 
-console.log(numeroSecreto);
+let numeroSecreto = 5;
 
-//Permite iteragir com a tela.
-let chute = prompt('Escolha um número entre 1 e 10:');
-console.log('Valor do chute: ', chute);
+//Enquato
+while (chute != numeroSecreto) {
+    //Permite iteragir com a tela.
+    chute = prompt('Escolha um número entre 1 e 10:');
 
-let numeroSecreto = 9;
-
-//Adicionado um console log para verificar a comparação entre chute e numeroSecreto.
-console.log('Resultado da comparação:', chute == numeroSecreto);
-
-//Se o chute for igual o numero secreto
-if( chute == numeroSecreto ){
-    alert(`Isso ai você descobriu o número secreto (${numeroSecreto}).`); //Template string.
-}else{
-    //Log para verifica o valor de "numeroScereto"
-    console.log('Valor do número secreto: ', numeroSecreto)
-    alert('O número secreto era ' + numeroSecreto);
+    //Se o chute for igual o numero secreto
+    if (chute == numeroSecreto) {
+        alert(`Isso ai você descobriu o número secreto (${numeroSecreto}).`); //Template string.
+    } else {
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`);
+        } else {
+            alert(`O número secreto é maior que ${chute}`)
+        }
+    }
 }

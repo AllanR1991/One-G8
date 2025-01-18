a480191dc77c83d1fcdd120dcbf89ce389a68b21
Allan Rodrigues dos Santos <allanrodrigues1991.ar@gmail.com>
2025-01-18 04:27:37 -0300

    feat: Crriado parte 3 do desafio 4 - contagem regressiva com base no numero informado.

--- 01 - Lógica de programação mergulhe em programação com JavaScript/04/logica-js-projeto_inicial/app.js
+++ 01 - Lógica de programação mergulhe em programação com JavaScript/04/logica-js-projeto_inicial/app.js
@@ -1,12 +1,25 @@
 let contador = 1;
 
-while(contador <= 100){
+while (contador <= 100) {
     console.log(contador)
     contador++;
 }
 
 contador = 10;
-while(contador>=0){
+while (contador >= 0) {
     console.log(contador)
     contador--;
 }
+
+let numero = 0;
+do {
+    numero = prompt('Digite um número para contagem regressiva: ');
+    if(numero>=0){
+        while (numero >= 0) {
+            console.log(numero)
+            numero--;
+        }
+        // Restaura o valor de 'numero' para evitar loop infinito no 'do...while' após a contagem regressiva.
+        numero+=1;
+    }
+} while (numero < 0)

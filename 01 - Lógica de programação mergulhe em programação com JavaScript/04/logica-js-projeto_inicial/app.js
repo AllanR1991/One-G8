e88ed7eb592897698da47e11a54c2bf933aecf75
Allan Rodrigues dos Santos <allanrodrigues1991.ar@gmail.com>
2025-01-19 00:59:39 -0300

    docs: adicionados comentários explicativos.

--- 01 - Lógica de programação mergulhe em programação com JavaScript/04/logica-js-projeto_inicial/app.js
+++ 01 - Lógica de programação mergulhe em programação com JavaScript/04/logica-js-projeto_inicial/app.js
@@ -1,39 +1,44 @@
+
+//Crie um contador que comece em 1 e vá até 10 usando um loop while. Mostre cada número.
 let contador = 1;
 
-while (contador <= 100) {
+while (contador <= 10) {
     console.log(contador)
     contador++;
 }
 
+//Crie um contador que começa em 10 e vá até 0 usando um loop while. Mostre cada número.
 contador = 10;
 while (contador >= 0) {
     console.log(contador)
     contador--;
 }
 
+//Crie um programa de contagem regressiva. Peça um número e conte deste número até 0, usando um loop while no console do navegador.
 let numero = 0;
 do {
     numero = prompt('Digite um número para contagem regressiva: ');
-    if(numero>=0){
+    if (numero >= 0) {
         while (numero >= 0) {
             console.log(numero)
             numero--;
         }
         // Restaura o valor de 'numero' para evitar loop infinito no 'do...while' após a contagem regressiva.
-        numero+=1;
+        numero += 1;
     }
 } while (numero < 0)
 
+//Crie um programa de contagem progressiva. Peça um número e conte de 0 até esse número, usando um loop while no console do navegador.
 
 contador = 0;
 do {
     numero = prompt('Digite um número para contagem progressiva: ');
-    if(numero>=0){
+    if (numero >= 0) {
         while (contador <= numero) {
             console.log(contador)
             contador++;
         }
         // Restaura o valor de 'numero' para evitar loop infinito no 'do...while' após a contagem regressiva.
-        
+
     }
 } while (numero < 0)

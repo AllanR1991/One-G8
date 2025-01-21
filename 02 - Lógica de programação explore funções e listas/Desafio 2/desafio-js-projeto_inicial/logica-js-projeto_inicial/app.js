1140bbb37f101f9e55c4d3e32fd9806df98500d6
Allan Rodrigues dos Santos <allanrodrigues1991.ar@gmail.com>
2025-01-20 21:59:58 -0300

    feat: resolvido a solução dos desafios propostos.

--- 02 - Lógica de programação explore funções e listas/Desafio 2/desafio-js-projeto_inicial/logica-js-projeto_inicial/app.js
+++ 02 - Lógica de programação explore funções e listas/Desafio 2/desafio-js-projeto_inicial/logica-js-projeto_inicial/app.js
@@ -12,3 +12,38 @@ Criar uma função que recebe dois números como parâmetros e retorna o maior d
 Criar uma função que recebe um número como parâmetro e retorna o resultado da multiplicação desse número por ele mesmo
 */
 
+function exibeOlaMundo(){
+    console.log('Olá mundo!');
+}
+
+function exibeOla(nome){
+    console.log(`Olá, ${nome}`);
+}
+
+function exibeODobro(numero){
+    return numero*2;
+}
+
+function media(num1,num2,num3){
+    return (num1+num2+num3)/3;
+}
+
+function numeroMaior(num1, num2){
+    if(num1>num2){
+        return num1;
+    }else{
+        return num2;
+    }
+}
+
+function multicacaoPorEleMesmo(num1){
+    return num1*num1;
+}
+
+exibeOlaMundo();
+exibeOla();
+console.log(exibeODobro(2));
+console.log(media(5,5,5));
+console.log(numeroMaior(10,60));
+console.log(numeroMaior(60,10));
+console.log(multicacaoPorEleMesmo(6));
